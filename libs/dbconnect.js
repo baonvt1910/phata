888c19b831769d3c0e27db9999ade13dd38aa297
baonvt1910 <84694936+baonvt1910@users.noreply.github.com>
2021-07-10 23:44:48 +0700

    Add files via upload

---
+++ libs/dbconnect.js
@@ -0,0 +1,46 @@
+// Nhớ cài Mongoose
+//  npm      i       --S      mongoose
+const mongoose = require("mongoose");
+
+
+/// Thông tin Database Collection / Tables
+/*
+const userName = "dbphathoang";
+const userPassword = "0909246357a";
+
+const dbName = "ATN_ShopToy";
+
+const url = "mongodb+srv://" +
+    userName + ":" + userPassword +
+    "@cluster0.r21sv.mongodb.net/" + dbName +
+    "?retryWrites=true&w=majority";
+*/
+
+const userName = "bao2002";
+const userPassword = "newpasss";
+
+const dbName = "ATN_shop";
+ 
+
+const url = "mongodb+srv://" +
+    userName + ":" + userPassword +     
+    "@cluster0.x8ieg.mongodb.net/" + dbName +
+    "?retryWrites=true&w=majority";
+function connectDB(xURL) {
+    // kết nối MongoDB bằng LIB Mongoose
+    mongoose.connect(xURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, (err) => {
+        if (err) {
+            console.log("\n Error !", err);
+        } else {
+            console.log("\n DB connected !");
+        }
+    });
+}
+
+module.exports = {
+    connectDB: connectDB,
+    xURL: url,
+}
